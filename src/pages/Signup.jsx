8b20191b15c0e39f67d173ca2e270a2337316244
Junaid Munir <junaid.munir-email@gmail.com>
2026-02-24 12:18:45 +0500

    add form handling, password validation, and Firebase authentication

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+
 import AuthLayout from "../component/Auth/AuthLayout";
 import { FcGoogle } from "react-icons/fc";
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../component/Auth/firebase";
+import { useState } from "react";
 const Signup = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if(password !== confirmPassword){
+      // alert("Passwords do not match!");
+      setError("Passwords do not match!");
+      return;
+    }
+
+    try{
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      console.log("User registered:", user);
+      console.log('user register successfully');
+    }catch(error){
+      console.error("Error registering user:", error);
+    }
+  }
+
   return (
     <AuthLayout
       title="Create Account 🚀"
       subtitle="Start managing your school efficiently"
     >
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={handleRegister}>
 
-        {/* Full Name */}
         <div>
           <label className="block text-sm font-medium text-gray-600">
             Full Name
@@ -18,6 +45,8 @@ const Signup = () => {
           <input
             type="text"
             placeholder="Enter your full name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#4D44B5] outline-none"
           />
         </div>
@@ -30,6 +59,8 @@ const Signup = () => {
           <input
             type="email"
             placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#4D44B5] outline-none"
           />
         </div>
@@ -42,6 +73,8 @@ const Signup = () => {
           <input
             type="password"
             placeholder="Create password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#4D44B5] outline-none"
           />
         </div>
@@ -54,6 +87,8 @@ const Signup = () => {
           <input
             type="password"
             placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#4D44B5] outline-none"
           />
         </div>
